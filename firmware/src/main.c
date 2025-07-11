177f8f063dcf44a2d375ffecc931d94ee22271a7
OneOfEleven <5f5bf935a51000f30c444f0acb434e72>
2025-07-11 11:59:36 +0100

    added dual/single ADC compile option

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -70,12 +70,14 @@ static const uint16_t omega_7x10[10] = {
 #if defined(USE_IWDG) && defined(HAL_IWDG_MODULE_ENABLED)
 	IWDG_HandleTypeDef hiwdg      = {0};
 #endif
-UART_HandleTypeDef huart1         = {0};
-DMA_HandleTypeDef  hdma_usart1_tx = {0};
-TIM_HandleTypeDef  htim3          = {0};
-ADC_HandleTypeDef  hadc1          = {0};
-ADC_HandleTypeDef  hadc2          = {0};
-DMA_HandleTypeDef  hdma_adc1      = {0};
+UART_HandleTypeDef     huart1         = {0};
+DMA_HandleTypeDef      hdma_usart1_tx = {0};
+TIM_HandleTypeDef      htim3          = {0};
+ADC_HandleTypeDef      hadc1          = {0};
+#ifdef DUAL_ADC_MODE
+	ADC_HandleTypeDef  hadc2          = {0};
+#endif
+DMA_HandleTypeDef      hdma_adc1      = {0};
 
 struct {
 	uint8_t por;
@@ -252,8 +254,13 @@ void set_DAC_amplitude(const float amplitude)
 
 void start_ADC(void)
 {
-	HAL_ADC_Start(&hadc2);
-	HAL_ADCEx_MultiModeStart_DMA(&hadc1, (uint32_t *)raw_adc_dma_data, DMA_ADC_DATA_LENGTH * 2);  // non-stop ADC double buffered sampling
+	// non-stop ADC double buffered sampling
+	#ifdef DUAL_ADC_MODE
+		HAL_ADC_Start(&hadc2);
+		HAL_ADCEx_MultiModeStart_DMA(&hadc1, (uint32_t *)raw_adc_dma_data, DMA_ADC_DATA_LENGTH * 2);
+	#else
+		HAL_ADC_Start_DMA(&hadc1, (uint32_t *)raw_adc_dma_data, DMA_ADC_DATA_LENGTH * 2);
+	#endif
 
 	sine_table_index = ARRAY_SIZE(sine_table) * 0.285; // align the sinewave (@1kHz)
 
@@ -263,8 +270,13 @@ void start_ADC(void)
 void stop_ADC(void)
 {
 	HAL_TIM_Base_Stop(&htim3);
-	HAL_ADCEx_MultiModeStop_DMA(&hadc1);
-	HAL_ADC_Stop(&hadc2);
+
+	#ifdef DUAL_ADC_MODE
+		HAL_ADCEx_MultiModeStop_DMA(&hadc1);
+		HAL_ADC_Stop(&hadc2);
+	#else
+		HAL_ADC_Stop_DMA(&hadc1);
+	#endif
 }
 
 // ***********************************************************
@@ -862,6 +874,15 @@ void process_Goertzel(void)
 	for (unsigned int buf_index = 0; buf_index < ARRAY_SIZE(adc_data); buf_index++)
 	{
 		#if !defined(GOERTZEL_FILTER_LENGTH) || (GOERTZEL_FILTER_LENGTH <= 0)
+			uint8_t filter = 0;
+		#else
+			uint8_t filter = 1;
+		#endif
+
+		if (!settings.uart_all_print_dso)
+			filter = 0;      // no need to fully filter if not sending the waveforms down the serial port
+
+		if (!filter)
 		{	// don't filter the waveform, do these ..
 			//    remove waveform DC offset
 			//   compute waveform RMS magnitude
@@ -904,10 +925,15 @@ void process_Goertzel(void)
 				phase_deg[buf_index] = (goertzel.re != 0.0f) ? fmodf((atan2f(goertzel.im, goertzel.re) * RAD_TO_DEG) + 270, 360) : NAN;
 			}
 		}
-		#else
+		else
 		{	// use Goertzel dft to filter the waveforms, length of filter is settable
 
 			const unsigned int filter_len = GOERTZEL_FILTER_LENGTH;
+
+			{	// compute waveform phase
+				goertzel_block(adc_data[buf_index], DMA_ADC_DATA_LENGTH, &goertzel);
+				phase_deg[buf_index] = (goertzel.re != 0.0f) ? fmodf((atan2f(goertzel.im, goertzel.re) * RAD_TO_DEG) + 270, 360) : NAN;
+			}
 
 			register t_comp *buf = tmp_buf;            // point to Goertzel dft output samples
 
@@ -944,11 +970,6 @@ void process_Goertzel(void)
 				}
 			}
 
-			{	// compute waveform phase using the 1st Goertzel I/Q output sample
-				const t_comp samp = buf[0];
-				phase_deg[buf_index] = (samp.re != 0.0f) ? fmodf((atan2f(samp.im, samp.re) * RAD_TO_DEG) + 270, 360) : NAN;
-			}
-
 			{	// compute RMS magnitude and save the Goertzel filtered output samples
 
 				register float *buf_out = adc_data[buf_index];
@@ -972,7 +993,6 @@ void process_Goertzel(void)
 				mag_rms[buf_index] = sqrtf(sum);
 			}
 		}
-		#endif
 	}
 
 	adc_avg_count++;
@@ -1786,22 +1806,25 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef *hadc)
 		hdma_adc1.Init.Direction           = DMA_PERIPH_TO_MEMORY;
 		hdma_adc1.Init.PeriphInc           = DMA_PINC_DISABLE;
 		hdma_adc1.Init.MemInc              = DMA_MINC_ENABLE;
-		hdma_adc1.Init.PeriphDataAlignment = DMA_PDATAALIGN_WORD; // For Dual ADC Mode
-		hdma_adc1.Init.MemDataAlignment    = DMA_MDATAALIGN_WORD;
+		#ifdef DUAL_ADC_MODE
+			hdma_adc1.Init.PeriphDataAlignment = DMA_PDATAALIGN_WORD;
+			hdma_adc1.Init.MemDataAlignment    = DMA_MDATAALIGN_WORD;
+		#else
+			hdma_adc1.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
+			hdma_adc1.Init.MemDataAlignment    = DMA_MDATAALIGN_HALFWORD;
+		#endif
 		hdma_adc1.Init.Mode                = DMA_CIRCULAR;
-		hdma_adc1.Init.Priority            = DMA_PRIORITY_VERY_HIGH;
+		hdma_adc1.Init.Priority            = DMA_PRIORITY_HIGH;
 		if (HAL_DMA_Init(&hdma_adc1) != HAL_OK)
-		{
 			Error_Handler();
-		}
 
 		__HAL_LINKDMA(hadc, DMA_Handle, hdma_adc1);
 	}
-
-	if (hadc->Instance == ADC2)
-	{
-		__HAL_RCC_ADC2_CLK_ENABLE();
-	}
+	#ifdef DUAL_ADC_MODE
+		else
+		if (hadc->Instance == ADC2)
+			__HAL_RCC_ADC2_CLK_ENABLE();
+	#endif
 }
 
 void HAL_ADC_MspDeInit(ADC_HandleTypeDef *hadc)
@@ -1809,16 +1832,19 @@ void HAL_ADC_MspDeInit(ADC_HandleTypeDef *hadc)
 	if (hadc->Instance == ADC1)
 	{
 		__HAL_RCC_ADC1_CLK_DISABLE();
-		HAL_GPIO_DeInit(GPIOA, GPIO_PIN_0 | GPIO_PIN_1);
+		HAL_GPIO_DeInit(ADC1_pin_GPIO_Port, ADC1_Pin);
+		HAL_GPIO_DeInit(ADC2_pin_GPIO_Port, ADC2_Pin);
 		HAL_DMA_DeInit(hadc->DMA_Handle);
 		HAL_NVIC_DisableIRQ(ADC1_2_IRQn);
 	}
-
-	if (hadc->Instance == ADC2)
-	{
-		__HAL_RCC_ADC2_CLK_DISABLE();
-		HAL_NVIC_DisableIRQ(ADC1_2_IRQn);
-	}
+	#ifdef DUAL_ADC_MODE
+		else
+		if (hadc->Instance == ADC2)
+		{
+			__HAL_RCC_ADC2_CLK_DISABLE();
+			HAL_NVIC_DisableIRQ(ADC1_2_IRQn);
+		}
+	#endif
 }
 
 void HAL_TIM_Base_MspInit(TIM_HandleTypeDef *htim_base)
@@ -1946,78 +1972,107 @@ void ADC_init(void)
 {
 	ADC_ChannelConfTypeDef sConfig = {0};
 
-	// ************************************************
-	// setup the master ADC
+	#ifdef DUAL_ADC_MODE
+		// simultaneous dual ADC mode
 
-	hadc1.Instance                   = ADC1;
-	hadc1.Init.ScanConvMode          = ADC_SCAN_DISABLE;
-	hadc1.Init.ContinuousConvMode    = DISABLE;
-	hadc1.Init.DiscontinuousConvMode = DISABLE;
-	hadc1.Init.ExternalTrigConv      = ADC_EXTERNALTRIGCONV_T3_TRGO;
-	hadc1.Init.DataAlign             = ADC_DATAALIGN_RIGHT;
-	hadc1.Init.NbrOfConversion       = 1;
-	if (HAL_ADC_Init(&hadc1) != HAL_OK)
-	{
-		Error_Handler();
-	}
+		// ************************************************
+		// setup the master ADC
 
-	//  Configure the ADC multi-mode
-	ADC_MultiModeTypeDef multimode = {0};
-	multimode.Mode                 = ADC_DUALMODE_REGSIMULT;
-	if (HAL_ADCEx_MultiModeConfigChannel(&hadc1, &multimode) != HAL_OK)
-	{
-		Error_Handler();
-	}
+		hadc1.Instance                   = ADC1;
+		hadc1.Init.ScanConvMode          = ADC_SCAN_DISABLE;
+		hadc1.Init.ContinuousConvMode    = DISABLE;
+		hadc1.Init.DiscontinuousConvMode = DISABLE;
+		hadc1.Init.ExternalTrigConv      = ADC_EXTERNALTRIGCONV_T3_TRGO;
+		hadc1.Init.DataAlign             = ADC_DATAALIGN_RIGHT;
+		hadc1.Init.NbrOfConversion       = 1;
+		if (HAL_ADC_Init(&hadc1) != HAL_OK)
+			Error_Handler();
 
-	// Configure Regular Channel
-	sConfig.Channel      = ADC_CHANNEL_0;                // PA0 Pin
-	sConfig.Rank         = ADC_REGULAR_RANK_1;
-//	sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLES_5;     //   1.5 + 12.5 =  14 cycles, ADC clk = 12MHz, 1.2us sample time, max 857kHz sample rate
-//	sConfig.SamplingTime = ADC_SAMPLETIME_7CYCLES_5;     //   7.5 + 12.5 =  20 cycles, ADC clk = 12MHz, 1.7us sample time, max 600kHz sample rate
-//	sConfig.SamplingTime = ADC_SAMPLETIME_13CYCLES_5;    //  13.5 + 12.5 =  26 cycles, ADC clk = 12MHz, 2.2us sample time, max 461kHz sample rate
-//	sConfig.SamplingTime = ADC_SAMPLETIME_28CYCLES_5;    //  28.5 + 12.5 =  41 cycles, ADC clk = 12MHz, 3.4us sample time, max 292kHz sample rate
-//	sConfig.SamplingTime = ADC_SAMPLETIME_41CYCLES_5;    //  41.5 + 12.5 =  54 cycles, ADC clk = 12MHz, 4.5us sample time, max 222kHz sample rate
-//	sConfig.SamplingTime = ADC_SAMPLETIME_55CYCLES_5;    //  55.5 + 12.5 =  68 cycles, ADC clk = 12MHz, 5.7us sample time, max 176kHz sample rate
-	sConfig.SamplingTime = ADC_SAMPLETIME_71CYCLES_5;    //  71.5 + 12.5 =  84 cycles, ADC clk = 12MHz, 7.0us sample time, max 142kHz sample rate
-//	sConfig.SamplingTime = ADC_SAMPLETIME_239CYCLES_5;   // 239.5 + 12.5 = 252 cycles, ADC clk = 12MHz, 21us sample time, max 47.6kHz sample rate
-	if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-	{
-		Error_Handler();
-	}
+		//  Configure the ADC multi-mode
+		ADC_MultiModeTypeDef multimode = {0};
+		multimode.Mode                 = ADC_DUALMODE_REGSIMULT;
+		if (HAL_ADCEx_MultiModeConfigChannel(&hadc1, &multimode) != HAL_OK)
+			Error_Handler();
 
-	// ************************************************
-	// setup the slave ADC
+//		sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLES_5;     //   1.5 + 12.5 =  14 cycles, ADC clk = 12MHz, 1.2us sample time, max 857kHz sample rate
+//		sConfig.SamplingTime = ADC_SAMPLETIME_7CYCLES_5;     //   7.5 + 12.5 =  20 cycles, ADC clk = 12MHz, 1.7us sample time, max 600kHz sample rate
+//		sConfig.SamplingTime = ADC_SAMPLETIME_13CYCLES_5;    //  13.5 + 12.5 =  26 cycles, ADC clk = 12MHz, 2.2us sample time, max 461kHz sample rate
+//		sConfig.SamplingTime = ADC_SAMPLETIME_28CYCLES_5;    //  28.5 + 12.5 =  41 cycles, ADC clk = 12MHz, 3.4us sample time, max 292kHz sample rate
+//		sConfig.SamplingTime = ADC_SAMPLETIME_41CYCLES_5;    //  41.5 + 12.5 =  54 cycles, ADC clk = 12MHz, 4.5us sample time, max 222kHz sample rate
+//		sConfig.SamplingTime = ADC_SAMPLETIME_55CYCLES_5;    //  55.5 + 12.5 =  68 cycles, ADC clk = 12MHz, 5.7us sample time, max 176kHz sample rate
+		sConfig.SamplingTime = ADC_SAMPLETIME_71CYCLES_5;    //  71.5 + 12.5 =  84 cycles, ADC clk = 12MHz, 7.0us sample time, max 142kHz sample rate
+//		sConfig.SamplingTime = ADC_SAMPLETIME_239CYCLES_5;   // 239.5 + 12.5 = 252 cycles, ADC clk = 12MHz, 21us sample time, max 47.6kHz sample rate
 
-	hadc2.Instance                = ADC2;
-	hadc2.Init                    = hadc1.Init;
-	hadc2.Init.ContinuousConvMode = ENABLE;              // **Very Important (Not Mentioned in any Document)
-	hadc2.Init.ExternalTrigConv   = ADC_SOFTWARE_START;
-	if (HAL_ADC_Init(&hadc2) != HAL_OK)
-	{
-		Error_Handler();
-	}
+		// Configure Regular Channel
+		sConfig.Channel = ADC_CHANNEL_0;                // PA0 Pin
+		sConfig.Rank    = ADC_REGULAR_RANK_1;
+		if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+			Error_Handler();
 
-	// Configure Regular Channel
-	sConfig.Channel        = ADC_CHANNEL_1;              // PA1 Pin
-	//sConfig.Rank         = ADC_REGULAR_RANK_1;
-	//sConfig.SamplingTime = ADC_SAMPLETIME_71CYCLES_5;
-	if (HAL_ADC_ConfigChannel(&hadc2, &sConfig) != HAL_OK)
-	{
-		Error_Handler();
-	}
+		// ************************************************
+		// setup the slave ADC
 
-	// ************************************************
-	// run the ADC calibrations
+		hadc2.Instance                = ADC2;
+		hadc2.Init                    = hadc1.Init;
+		hadc2.Init.ContinuousConvMode = ENABLE;              // **Very Important (Not Mentioned in any Document)
+		hadc2.Init.ExternalTrigConv   = ADC_SOFTWARE_START;
+		if (HAL_ADC_Init(&hadc2) != HAL_OK)
+			Error_Handler();
 
-	if (HAL_ADCEx_Calibration_Start(&hadc1) != HAL_OK)
-	{
-		Error_Handler();
-	}
+		// Configure Regular Channel
+		sConfig.Channel = ADC_CHANNEL_1;              // PA1 Pin
+		sConfig.Rank    = ADC_REGULAR_RANK_1;
+		if (HAL_ADC_ConfigChannel(&hadc2, &sConfig) != HAL_OK)
+			Error_Handler();
 
-	if (HAL_ADCEx_Calibration_Start(&hadc2) != HAL_OK)
-	{
-		Error_Handler();
-	}
+		// ************************************************
+		// run the ADC calibrations
+
+		if (HAL_ADCEx_Calibration_Start(&hadc1) != HAL_OK)
+			Error_Handler();
+
+		if (HAL_ADCEx_Calibration_Start(&hadc2) != HAL_OK)
+			Error_Handler();
+		
+	#else
+		// single ADC dual channel mode
+
+		hadc1.Instance                   = ADC1;
+		hadc1.Init.ScanConvMode          = ADC_SCAN_ENABLE;
+		hadc1.Init.ContinuousConvMode    = DISABLE;
+		hadc1.Init.DiscontinuousConvMode = DISABLE;
+		hadc1.Init.ExternalTrigConv      = ADC_EXTERNALTRIGCONV_T3_TRGO;
+		hadc1.Init.DataAlign             = ADC_DATAALIGN_RIGHT;
+		hadc1.Init.NbrOfConversion       = 2;
+		if (HAL_ADC_Init(&hadc1) != HAL_OK)
+			Error_Handler();
+
+//		sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLES_5;     //   1.5 + 12.5 =  14 cycles, ADC clk = 12MHz, 1.2us sample time, max 857kHz sample rate
+//		sConfig.SamplingTime = ADC_SAMPLETIME_7CYCLES_5;     //   7.5 + 12.5 =  20 cycles, ADC clk = 12MHz, 1.7us sample time, max 600kHz sample rate
+//		sConfig.SamplingTime = ADC_SAMPLETIME_13CYCLES_5;    //  13.5 + 12.5 =  26 cycles, ADC clk = 12MHz, 2.2us sample time, max 461kHz sample rate
+//		sConfig.SamplingTime = ADC_SAMPLETIME_28CYCLES_5;    //  28.5 + 12.5 =  41 cycles, ADC clk = 12MHz, 3.4us sample time, max 292kHz sample rate
+//		sConfig.SamplingTime = ADC_SAMPLETIME_41CYCLES_5;    //  41.5 + 12.5 =  54 cycles, ADC clk = 12MHz, 4.5us sample time, max 222kHz sample rate
+//		sConfig.SamplingTime = ADC_SAMPLETIME_55CYCLES_5;    //  55.5 + 12.5 =  68 cycles, ADC clk = 12MHz, 5.7us sample time, max 176kHz sample rate
+		sConfig.SamplingTime = ADC_SAMPLETIME_71CYCLES_5;    //  71.5 + 12.5 =  84 cycles, ADC clk = 12MHz, 7.0us sample time, max 142kHz sample rate
+//		sConfig.SamplingTime = ADC_SAMPLETIME_239CYCLES_5;   // 239.5 + 12.5 = 252 cycles, ADC clk = 12MHz, 21us sample time, max 47.6kHz sample rate
+
+		sConfig.Channel = ADC_CHANNEL_0;                // PA0 Pin
+		sConfig.Rank    = ADC_REGULAR_RANK_1;
+		if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+			Error_Handler();
+
+		sConfig.Channel = ADC_CHANNEL_1;                // PA1 Pin
+		sConfig.Rank    = ADC_REGULAR_RANK_2;
+		if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+			Error_Handler();
+
+		// ************************************************
+		// run the ADC calibration
+
+		if (HAL_ADCEx_Calibration_Start(&hadc1) != HAL_OK)
+			Error_Handler();
+
+	#endif
 }
 
 void DMA_init(void)
